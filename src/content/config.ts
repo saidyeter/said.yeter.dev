fe80f43137fdfa09ac4ce7027261b71ea34e6f74
Said Yeter <kordiseps@gmail.com>
2023-07-02 17:41:50 +0300

    Recreate template #1

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,42 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+// const blog = defineCollection({
+//     // Type-check frontmatter using a schema
+//     schema: z.object({
+//         title: z.string(),
+//         description: z.string(),
+//         // Transform string to Date object
+//         pubDate: z
+//             .string()
+//             .or(z.date())
+//             .transform((val) => new Date(val)),
+//         updatedDate: z
+//             .string()
+//             .optional()
+//             .transform((str) => (str ? new Date(str) : undefined)),
+//         heroImage: z.string().optional(),
+//     }),
+// });
+
+
+const work = defineCollection({
+    // Type-check frontmatter using a schema
+    schema: z.object({
+
+        companyName: z.string(),
+        companyLink: z.string(),
+        companyLocation: z.string(),
+        companyBusiness: z.string(),
+        companyImage: z.string(),
+        title: z.string(),
+        jobType: z.string(),
+        workType: z.string(),
+        totalWorkTime: z.string(),
+        startDate: z.string(),
+        endDate: z.string(),
+        tags: z.string().array(),
+        order: z.number()
+    }),
 });
 
-export const collections = { blog };
+export const collections = {  work };
---
+++ src/content/work/super-entegrator.md
@@ -0,0 +1,19 @@
+---
+companyName: 'Super Entegrator'
+companyLink: 'https://www.superentegrator.com/'
+companyLocation: 'TR Istanbul Atasehir'
+companyBusiness: 'Fintech'
+companyImage: '/Super-Entegrator-Logo-01.png'
+title: 'Software Engineer'
+jobType: 'Full Time'
+workType: 'Remote'
+totalWorkTime: '16 Months'
+startDate: 'February 2021'
+endDate: 'May 2022'
+tags: [".NET C#", "PostgreSQL", "Redis", "RabbitMQ", "MongoDB", "Azure DevOps", "Microservices", "Web API", "WCF Services", "Hosted Services", "Docker", "Rancher"]
+order: 1
+---
+
+# Super Entegrator
+
+Super Entegrator is a startup. I joined Super Entegrator in the early stage. There was no customer. We build the product from scratch. The business was invoicing, despatching, ledger management, and other small digital transformation products. We used .Net Core to build services, RabbitMq for communication between services, PostgreSQL to store data, Redis for caching, ElasticSearch for logging. There were also .Net Framework WCF services for communicating with TR Revenue Administration
